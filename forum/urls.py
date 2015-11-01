f2445c30704d76cb0bedf5a9f0f4d78b47a577b7
victornicolet <victor.nicolet.0@gmail.com>
2015-11-01 17:17:48 +0100

    Use helper tags (not yet working!) + links on homepage and forum page

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ThreadCategoryListView.as_view())
+    url(r'^$', views.ThreadCategoryListView.as_view(), name='forum')
 ]
