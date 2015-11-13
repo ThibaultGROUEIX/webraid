c76aa03709d131fac37e551aea06f624ca4bc86d
victornicolet <victor.nicolet.0@gmail.com>
2015-11-13 17:41:58 +0100

    You can now edit threads

--- forum/urls.py
+++ forum/urls.py
@@ -5,12 +5,19 @@ from . import views
 urlpatterns = [
     url(r'^$',
         views.ThreadCategoryListView.as_view(), name='forum'),
+
     url(r'^add_category/$',
         views.AddThreadCategoryView.as_view(), name='add_thread_category'),
+
     url(r'^category/(?P<slug>[\w-]+)/$',
         views.thread_category_detail, name='detail_thread_category'),
+
+    url(r'^category/(?P<slug>[\w-]+)/edit/(?P<pk>\d+)/$',
+        views.thread_category_detail, name='detail_thread_category_edit_thread'),
+
     url(r'^category/(?P<category_slug>[\w-]+)/thread/(?P<slug>[\w-]+)/$',
         views.thread_detail, name='detail_thread'),
+
     url(r'^category/(?P<category_slug>[\w-]+)/thread/(?P<slug>[\w-]+)/edit/(?P<pk>\d+)/$',
         views.thread_detail, name='detail_thread_edit_post'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView, FormView
 from django.http import Http404
 from django.shortcuts import redirect, render
-from django.forms.models import model_to_dict
 
 from profiles.models import UserProfile
 from models import ThreadCategory, Thread, Post
@@ -23,7 +22,12 @@ class AddThreadCategoryView(FormView):
         return super(AddThreadCategoryView, self).form_valid(form)
 
 
-def thread_category_detail(request, slug):
+def thread_category_detail(request, slug, pk=None):
+    if pk is not None:
+        edit_thread = Thread.objects.get(pk=pk)
+    else:
+        edit_thread = None
+
     try:
         thread_category = ThreadCategory.objects.get(slug=slug)
         thread_list = Thread.objects.filter(category=thread_category.pk)
@@ -31,32 +35,59 @@ def thread_category_detail(request, slug):
         raise Http404("Thread category does not exist!")
 
     if request.method == 'POST':
-        form = ThreadForm(request.POST)
-        if form.is_valid():
-            thread = form.save(commit=False)
-            thread.category = thread_category
-            thread.save()
 
-            return redirect(thread_category.get_absolute_url())
-        else:
-            return render(request,
-                          'thread_category.html',
-                          {
-                              'thread_category': thread_category,
-                              'thread_list': thread_list,
-                              'form': form,
-                              'slug': slug,
-                          })
+        if 'new-thread' in request.POST:
+            form = ThreadForm(request.POST)
+            if form.is_valid():
+                thread = form.save(commit=False)
+                thread.category = thread_category
+                thread.save()
+
+                return redirect(thread_category.get_absolute_url())
+            else:
+                return render(request,
+                              'thread_category.html',
+                              {
+                                  'thread_category': thread_category,
+                                  'thread_list': thread_list,
+                                  'form': form,
+                                  'slug': slug,
+                              })
+        elif 'edit-thread' in request.POST:
+            edit_thread_form = ThreadForm(request.POST, instance=edit_thread)
+            if edit_thread_form.is_valid():
+                edit_thread_form.save()
+
+                return redirect(thread_category.get_absolute_url())
+            else:
+                return render(request,
+                              'thread_category.html',
+                              {
+                                  'thread_category': thread_category,
+                                  'thread_list': thread_list,
+                                  'form': ThreadForm(),
+                                  'slug': slug,
+                                  'pk': edit_thread.pk,
+                                  'edit_thread_form': edit_thread_form,
+                              })
 
     form = ThreadForm()
-    return render(request,
-                  'thread_category.html',
-                  {
-                      'thread_category': thread_category,
-                      'thread_list': thread_list,
-                      'form': form,
-                      'slug': slug,
-                  })
+
+    data = {
+        'thread_category': thread_category,
+        'thread_list': thread_list,
+        'form': form,
+        'slug': slug,
+    }
+
+    if edit_thread is not None:
+        edit_thread_form = ThreadForm(instance=edit_thread)
+
+        data['pk'] = edit_thread.pk;
+        data['edit_thread_form'] = edit_thread_form;
+        data['edit_thread'] = edit_thread
+
+    return render(request, 'thread_category.html', data)
 
 
 def thread_detail(request, category_slug, slug, pk=None):
