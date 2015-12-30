d9c1110555ec23ffe5d3ed07661d011abd499480
victornicolet <victor.nicolet.0@gmail.com>
2015-12-30 21:58:30 +0100

    Better urls in forum

--- forum/urls.py
+++ forum/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     url(r'^add_category/$',
         views.AddThreadCategoryView.as_view(), name='add_thread_category'),
 
-    url(r'^category/(?P<slug>[\w-]+)/$',
+    url(r'^(?P<slug>[\w-]+)/$',
         views.thread_category_detail, name='detail_thread_category'),
 
-    url(r'^category/(?P<slug>[\w-]+)/edit/(?P<pk>\d+)/$',
+    url(r'^/(?P<slug>[\w-]+)/(?P<pk>\d+)/edit$',
         views.thread_category_detail, name='detail_thread_category_edit_thread'),
 
-    url(r'^category/(?P<category_slug>[\w-]+)/thread/(?P<slug>[\w-]+)/$',
+    url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$',
         views.thread_detail, name='detail_thread'),
 
-    url(r'^category/(?P<category_slug>[\w-]+)/thread/(?P<slug>[\w-]+)/edit/(?P<pk>\d+)/$',
+    url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/(?P<pk>\d+)/edit$',
         views.thread_detail, name='detail_thread_edit_post'),
 ]
