46dc248bce35b895d2829ddfdbf911565d84ad40
Nicolet Victor <victornicolet@users.noreply.github.com>
2016-01-18 14:36:03 +0100

    Revert "Usability"

--- compile_less.sh
+++
@@ -1,2 +0,0 @@
-lessc static/less/style.less static/css/style.css
-lessc --clean-css static/less/bootstrap-select.less static/css/bootstrap-select.min.css
--- forum/urls.py
+++ forum/urls.py
@@ -9,27 +9,15 @@ urlpatterns = [
     url(r'^add_category/$',
         views.AddThreadCategoryView.as_view(), name='add_thread_category'),
 
-    url(r'^(?P<slug>[\w-]+)/$',
+    url(r'^category/(?P<slug>[\w-]+)/$',
         views.thread_category_detail, name='detail_thread_category'),
 
-    url(r'^/categories/(?P<slug>[\w-]+)/delete',
-        views.category_delete,
-        name='delete_category'),
-
-    url(r'^/(?P<slug>[\w-]+)/(?P<pk>\d+)/edit$',
+    url(r'^category/(?P<slug>[\w-]+)/edit/(?P<pk>\d+)/$',
         views.thread_category_detail, name='detail_thread_category_edit_thread'),
 
-    url(r'^/threads/(?P<slug>[\w-]+)/delete',
-        views.thread_delete,
-        name='delete_thread'),
-
-    url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$',
+    url(r'^category/(?P<category_slug>[\w-]+)/thread/(?P<slug>[\w-]+)/$',
         views.thread_detail, name='detail_thread'),
 
-    url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/(?P<pk>\d+)/edit$',
+    url(r'^category/(?P<category_slug>[\w-]+)/thread/(?P<slug>[\w-]+)/edit/(?P<pk>\d+)/$',
         views.thread_detail, name='detail_thread_edit_post'),
-
-    url(r'delete_post/(?P<post_id>\d+)$',
-        views.post_delete,
-        name='detail_thread_delete_post')
 ]
